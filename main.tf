4e58dbf21dd098eed04ac7ed519310b2f469b3c8
Derek Major <major1@Dereks-MacBook-Air.local>
2025-11-13 22:31:23 -0500

    increase lambda timeout

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "xrpl_proxy" {
   project_name   = "xrpl-rpc-proxy"
   environment    = "prod"
   rpc_endpoints  = [
-    "wss://stylish-broken-reel.xrp-mainnet.quiknode.pro/cbb31b9a639b2f38f2c7aa36635302e0c493bfcb/"
+    "wss://stylish-broken-reel.xrp-mainnet.quiknode.pro/cbb31b9a639b2f38f2c7aa36635302e0c493bfcb"
   ]
   rate_limit_rps = 10
   burst_limit    = 20
